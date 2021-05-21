e8677c5f833c49bd8e2a9236e343c7c43c0d2a48
ahmadammmoura <ammoura575@gmail.com>
2021-05-21 19:27:23 +0300

    done today

---
+++ src/API/requests.js
@@ -0,0 +1,18 @@
+
+const API_KEY = process.env.REACT_APP_MOVIES_DB_KEY
+
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTopTvShows:`/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    // fetchTVcridets : `/tv/${5}/credits?api_key=&${API_KEY}language=en-US`
+};
+
+ 
+export default requests;

41aae2a016e4852fd3064acfa00b8e094af60f88
AbeerAl-Rafati <Abeerw.rafati@yahoo.com>
2021-05-24 15:32:18 +0300

    get and delete function

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,75 +1,96 @@
-// import axios from 'axios';
-import React, { Component } from 'react';
-import NavBar from './NavBar'
-import { withAuth0 } from '@auth0/auth0-react';
-
+import axios from "axios";
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import { withAuth0 } from "@auth0/auth0-react";
+// import "../style/watchlist.css";
+// import Img from '../img/a.jpg'
 class WatchList extends Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
+    const { user } = this.props.auth0;
 
-  //   this.state = {
-  //     watchListMovies: [],
-  //     showMovies: false,
-  //     userEmail: '',
-  //     server: process.env.REACT_APP_SERVER_URL,
-  //   }
-  // }
-  // getWatchListMovies = async () => {
-  //   const { user } = this.props.auth0;
-  //   try {
-  //     const params = {
-  //       email: user.email
-  //     }
-  //     const moviesList = await axios.get(`${this.state.server}/movies`, { params: params });
-  //     this.setState({
-  //       watchListMovies: moviesList.data[0].movies,
-  //       showMovies: true,
-  //       email: user.email
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+    this.state = {
+      watchListMovies: [],
+      showMovies: false,
+      userEmail: user.email,
+      server: process.env.REACT_APP_SERVER_URL,
+      // index: 0
+    };
+  }
 
-  // deleteMovie = async (index) => {
-  //   const { user } = this.props.auth0;
-  //   const newMovieList = this.state.watchListMovies.filter((movie, idx) => {
-  //     return idx !== index;
-  //   });
-  //   const query = {
-  //     email: user.email
-  //   }
+  componentDidMount = async () => {
+    try {
+      const params = {
+        email: this.state.userEmail,
+      };
 
-  //   await axios.delete(`${this.state.server}/movies/${index}`, { params: query });
+      const moviesList = await axios.get(`${this.state.server}/movies`, { params: params });
 
-  // }
+      this.setState({
+        watchListMovies: moviesList.data.movies,
+        showMovies: true,
+      });
+      console.log('this is what u are lookin for', moviesList);
+      console.log("what is this:", this.state.watchListMovies);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  render() {
-    // const baseImgUrl = "https://image.tmdb.org/t/p/original";
-    // const moviesList = this.state.watchListMovies.map(movie => {
-      return (
-        <>
-          <NavBar/>
-          <div className="movies__container">
-            <div className="movie" style={{backgroundImage: `url(https://ae01.alicdn.com/kf/HTB1xKI9PFXXXXXAXVXXq6xXFXXXG/Dropship-The-Shawshank-Redemption-Nostalgia-classic-movie-kraft-paper-bar-poster-Retro-Poster-decorative-painting.jpg)` , width: '35rem', height: '35rem'}  }>
-              <img className="card-img-top" src={'https://ae01.alicdn.com/kf/HTB1xKI9PFXXXXXAXVXXq6xXFXXXG/Dropship-The-Shawshank-Redemption-Nostalgia-classic-movie-kraft-paper-bar-poster-Retro-Poster-decorative-painting.jpg'} alt={'movie1'} style={{ width: '10rem' }} />
-              <div className="movie__infos">
-                <h2 className="movie__title">The Shawshank Redemption</h2>
-                <h4 className="movie__year" style={{ fontSize: '20px' }}> Year : 1994</h4>
-                <h4 className="movie__director" style={{ fontSize: '20px' }}>Director :  Frank Darabont </h4>
-                <span className ="movie__duration" style={{ fontSize: '20px' }} > Duration : 142 min </span>
-                <br/>
-                <span className ="Rating" style={{ fontSize: '20px' }}> Rating : 9.3 </span>
-                <p className="movie__description" >Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.</p>  
-                <div className ="movie__button"style={{ fontSize: '20px' }}>
-                  <button onClick={() => { }}> Delete </button>
-                </div> 
+  deleteMovie = async (index) => {
+
+    const newMovieList = this.state.watchListMovies.filter((movie, idx) => {
+      return idx !== index;
+    });
+
+    this.setState({
+      watchListMovies: newMovieList,
+
+    });
+
+    const query = {
+      email: this.state.userEmail
+    }
+
+    await axios.delete(`${this.state.server}/movies/${index}`, { params: query });
+
+  }
+
+  render() // const baseImgUrl = "https://image.tmdb.org/t/p/original";
+  {
+
+
+    return (
+      <>
+        <NavBar />
+        <h1 style={{ color: "black", margin: "5rem" }}>Wish List</h1>
+        {this.state.watchListMovies.map((movie, idx) => {
+          return (
+            <>
+
+
+              <div id="card_container" data-offset="2">
+                <div className="pg"></div>
+                <div id="card">
+                  <div className="shine"></div>
+                  <div className="text-block">
+                    <h1>
+                      {movie.name}
+                      <small>{movie.year}</small>
+                    </h1>
+                    <h2>{movie.duration}</h2>
+                    <p>
+                      {movie.description}
+                    </p>
+                    <button onClick={() => { this.deleteMovie(idx) }}> Delete </button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </>
-      );
-    // });
+            </>
+          );
+        })}
+      </>
+    );
   }
 }
 export default withAuth0(WatchList);
